057e7d4b1f50de5d73e6718fe5127cbfb22c9493
Avro Biswas <70520795+avro1199@users.noreply.github.com>
2025-07-11 01:59:51 +0600

    Delete libraries directory

--- libraries/EasyPreferences/examples/lib/basic-keys/preferences-keys.h
+++
@@ -1,7 +0,0 @@
-#define CONFIG_KEYS_LIST        \
-  X(KPWRON, "PowerOn", INT)     \
-  X(KPINUM, "PiNumber", FLOAT)  \
-  X(KMYINT, "MyInteger", INT)   \
-  X(KMYSTR, "MyString", STRING) \
-  X(KMBOOL, "MyBool", BOOL)     \
-  X(KCOUNT, "KCOUNT", UNKNOWN)
