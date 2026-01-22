cc3eb8cff201aaa972933de4fc7915be11c943d3
Rj Avro Biswas <avro1199@gmail.com>
2026-01-22 22:16:51 +0600

    Soccer_Final Imrroved

--- Soccer_Final/Soccer_Final.ino
+++ Soccer_Final/Soccer_Final.ino
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+#define DEBUG
+
 // -------------------- Motor PWM (LEDC) --------------------
 static const uint8_t LEFT_MOTOR_FORWARD = 13;
 static const uint8_t LEFT_MOTOR_BACKWARD = 12;
@@ -10,7 +12,6 @@ static const uint8_t RIGHT_MOTOR_BACKWARD = 25;
 
 // Debug options
 static const uint8_t LED = 2;
-
 
 // LEDC configuration
 static const uint32_t PWM_FREQ_HZ = 20000; // 20 kHz
@@ -41,8 +42,7 @@ static uint8_t idx = 0;
 
 // Speed control
 static uint16_t TOP_SPEED = 0;
-static uint16_t BRAKE_THR = 1400; // RC value below which braking is applied
-
+static uint16_t BRAKE_THRESHOLD = 1300; // RC value below which braking is applied
 
 void setup()
 {
@@ -74,6 +74,7 @@ void setup()
 void loop()
 {
     static uint16_t ch[14];
+    int leftSpeed, rightSpeed;
 
     // Parse frames as they arrive; update motor outputs immediately on a valid frame.
     if (readIbusFrame(ch))
@@ -83,7 +84,14 @@ void loop()
         // Channel indexing: ch[0]=CH1, ch[1]=CH2, ...
         TOP_SPEED = (uint16_t)map((int)ch[2], RC_MIN, RC_MAX, 0, PWM_MAX); // CH3 for top speed limit
 
-        if(ch[3] < BRAKE_THR) // CH4 for e-stop
+        if (TOP_SPEED < 10)
+        {
+            allMotorsNeutral();
+            digitalWrite(LED, HIGH); // Indicate no speed
+            return;
+        }
+
+        if (ch[3] < BRAKE_THRESHOLD) // CH4 for e-stop
         {
             allMotorsBrake();
             digitalWrite(LED, HIGH); // Indicate braking
@@ -92,20 +100,31 @@ void loop()
 
         digitalWrite(LED, LOW); // Normal operation
 
-        // No differential mix (i.e. leftSpeed = CH1+CH2, rightSpeed = CH1-CH2)
-        // Mix is done in the transmitter for better control.
-        // Here, just map RC to speed directly for each motor (CH1=left, CH2=right).
+        // Differential mix (i.e. leftSpeed = CH2+CH1, rightSpeed = CH2-CH1)
+        // Mix can be done in the transmitter for better control.
+        int steering = rcToSpeed((int)ch[0]); // CH1
+        int throttle = rcToSpeed((int)ch[1]); // CH2
 
-        int leftSpeed = rcToSpeed((int)ch[0]); // CH1
-        int rightSpeed = rcToSpeed((int)ch[1]); // CH2
+        if (ch[7] > 1500 && throttle < 100) // CH8 for reverse
+        {
+            leftSpeed = constrain((throttle - steering), -((int)TOP_SPEED), (int)TOP_SPEED);
+            rightSpeed = constrain((throttle + steering), -((int)TOP_SPEED), (int)TOP_SPEED);
+        }
+        else
+        {
+            leftSpeed = constrain((throttle + steering), -((int)TOP_SPEED), (int)TOP_SPEED);
+            rightSpeed = constrain((throttle - steering), -((int)TOP_SPEED), (int)TOP_SPEED);
+        }
 
-        // setMotor(leftSpeed, rightSpeed);
-        // Checking in Serial Monitor
+        setMotor(leftSpeed, rightSpeed);
+
+// Checking in Serial Monitor
+#ifdef DEBUG
         Serial.print("Left Speed: ");
         Serial.print(leftSpeed);
         Serial.print(" | Right Speed: ");
         Serial.println(rightSpeed);
-
+#endif
     }
 
     // Failsafe: stop if signal lost
